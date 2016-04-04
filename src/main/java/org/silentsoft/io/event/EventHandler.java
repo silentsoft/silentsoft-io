1d753f1160fb4fd7667b5d17dbcb1b64414c28c6
silentsoft <hs830.lee@gmail.com>
2016-04-04 20:47:11 +0900

    traceable event history

--- src/main/java/org/silentsoft/io/event/EventHandler.java
+++ src/main/java/org/silentsoft/io/event/EventHandler.java
@@ -5,37 +5,78 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.silentsoft.io.event.EventListener;
+import org.silentsoft.io.event.EventHistory.EventType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class EventHandler {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EventHandler.class);
 	
 	private static final int MAX_THREAD_POOL = 5;
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(EventHandler.class);
+	private static int historyCapacity = 0;
 	
-	/**
-	 * Note : ArrayList may occur ConcurrentModificationException
-	 * 		  so using CopyOnWriteArrayList for prevent Exception multi thread.
-	 * Do not use below source code.
-	 * private static List<EventListener> listeners = new ArrayList<EventListener>();
-	 */
-	private static List<EventListener> listeners = new CopyOnWriteArrayList<EventListener>();
+	private static List<EventHistory> histories;
+	
+	private static List<EventListener> listeners;
+	
+	static {
+		/**
+		 * Note : ArrayList may occur ConcurrentModificationException
+		 * 		  so using CopyOnWriteArrayList for prevent Exception multi thread.
+		 * Do not use below source code.
+		 * private static List<EventListener> listeners = new ArrayList<EventListener>();
+		 */
+		
+		histories = new CopyOnWriteArrayList<EventHistory>();
+		listeners = new CopyOnWriteArrayList<EventListener>();
+		
+		initHistory(100);
+	}
+	
+	public static synchronized void initHistory(int capacity) {
+		getHistories().clear();
+		setHistoryCapacity(capacity);
+	}
+	
+	public static synchronized List<EventHistory> getHistories() {
+		return histories;
+	}
+	
+	private static synchronized void setHistoryCapacity(int capacity) {
+		historyCapacity = capacity;
+	}
+	
+	private static synchronized void addHistory(EventHistory eventHistory) {
+		if (getHistoryCapacity() > 0) {
+			if (getHistories().size() >= getHistoryCapacity()) {
+				getHistories().remove(0);
+			}
+			
+			getHistories().add(eventHistory);
+		}
+		
+		eventHistory = null;
+	}
+	
+	private static synchronized int getHistoryCapacity() {
+		return historyCapacity;
+	}
+	
+	public static synchronized void addListener(EventListener eventListener) {
+		if (getListeners().indexOf(eventListener) == -1) {
+			getListeners().add(eventListener);
+		}
+	}
 	
 	private static synchronized List<EventListener> getListeners() {
 		return listeners;
 	}
 	
-	public static synchronized void addListener(EventListener eventListener) {
-		if (getListeners().indexOf(eventListener) == -1) {
-			listeners.add(eventListener);
-		}
-	}
-	
 	public static synchronized void removeListener(EventListener eventListener) {
 		if (getListeners().indexOf(eventListener) != -1) {
-			listeners.remove(eventListener);
+			getListeners().remove(eventListener);
 		}
 	}
 	
@@ -56,6 +97,9 @@ public final class EventHandler {
 	 * @param doAsynch
 	 */
 	public static synchronized void callEvent(final Class<?> caller, String event, boolean doAsynch) {
+		addHistory(new EventHistory(EventType.OCCUR, caller, event, null, doAsynch));
+		LOGGER.info("[Event occur : <{}> by <{}>]", new Object[]{event, caller.getName()});
+		
 		if (doAsynch) {
 			callEventByAsynch(caller, event);
 		} else {
@@ -66,46 +110,40 @@ public final class EventHandler {
 	private static synchronized void callEventByAsynch(final Class<?> caller, final String event) {
 		ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREAD_POOL);
 		
-		LOGGER.info("[Event occur : <{}> by <{}>]", new Object[]{event, caller.getName()});
-		
 		for (final EventListener listener : listeners) {
-			executorService.execute(
-				new Runnable() {
-					public void run() {
-						if (listener.getClass().getName().equals(caller.getName())) {
-			                LOGGER.info("[Event skip : <{}> by self <{}>]", new Object[]{event, caller.getName()});
-			            } else {
-			                LOGGER.info("[Event catch : <{}> by <{}>]", new Object[]{event, listener.getClass().getName()});
-			                
-			                try {
-			                	listener.onEvent(event);
-			                } catch (Exception e) {
-			                	LOGGER.error("[Event error : <{}> on <{}>]", new Object[]{event, listener.getClass().getName(), e});
-			                }
-			            }
-					}
-				}
-			);
+			executorService.execute(createEventStub(caller, event, listener, true));
         }
 		
 		executorService.shutdown();
 	}
 	
 	private static synchronized void callEventBySynch(final Class<?> caller, final String event) {
-		LOGGER.info("[Event occur : <{}> by <{}>]", new Object[]{event, caller.getName()});
-		
 		for (final EventListener listener : listeners) {
-			if (listener.getClass().getName().equals(caller.getName())) {
-                LOGGER.info("[Event skip : <{}> by self <{}>]", new Object[]{event, caller.getName()});
-            } else {
-                LOGGER.info("[Event catch : <{}> by <{}>]", new Object[]{event, listener.getClass().getName()});
-                
-                try {
-                	listener.onEvent(event);
-                } catch (Exception e) {
-                	LOGGER.error("[Event error : <{}> on <{}>]", new Object[]{event, listener.getClass().getName(), e});
-                }
-            }
+			createEventStub(caller, event, listener, false).run();
         }
+	}
+	
+	private static synchronized Runnable createEventStub(Class<?> caller, String event, EventListener listener, boolean isAsynch) {
+		return new Runnable() {
+			@Override
+			public void run() {
+				if (listener.getClass().getName().equals(caller.getName())) {
+					addHistory(new EventHistory(EventType.SKIP, caller, event, listener, isAsynch));
+	                LOGGER.info("[Event skip : <{}> by self <{}>]", new Object[]{event, caller.getName()});
+	            } else {
+	            	addHistory(new EventHistory(EventType.CATCH, caller, event, listener, isAsynch));
+	                LOGGER.info("[Event catch : <{}> by <{}>]", new Object[]{event, listener.getClass().getName()});
+	                
+	                try {
+	                	addHistory(new EventHistory(EventType.SCHEDULED, caller, event, listener, isAsynch));
+	                	listener.onEvent(event);
+	                	addHistory(new EventHistory(EventType.PROCESSED, caller, event, listener, isAsynch));
+	                } catch (Exception e) {
+	                	addHistory(new EventHistory(EventType.FAILURE, caller, event, listener, isAsynch));
+	                	LOGGER.error("[Event failure : <{}> on <{}>]", new Object[]{event, listener.getClass().getName(), e});
+	                }
+	            }
+			}
+		};
 	}
 }
